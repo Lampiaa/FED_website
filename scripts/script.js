8865fc8abc625e6d4cd04b337f29952e95bdbcff
Lampiaa <150828818+Lampiaa@users.noreply.github.com>
2025-10-02 02:12:47 +0200

    beetje css index

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
 console.log("hi");
+
+
+////////////////////
+// Hamburger menu //
+////////////////////
+
+let deButton = document.querySelector("button")
+
+deButton.addEventListener("click", toggleButton)
+
+function toggleButton(){
+  deButton.classList.toggle("is-open")
+}
